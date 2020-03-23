99caa0ded9d5e61bf2bbdd49f6f78b405b8bd8fd
Luis Sousa <luis.sousa@dotlogic.pt>
2020-03-23 12:00:00 +0000

    fixed store dispatch requests on index.html

--- pages/index.vue
+++ pages/index.vue
@@ -13,42 +13,42 @@
 import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
 
 export default {
-  data() {
-    return {
-      jogo: {}
-    }
-  },
-  async fetch({ store }) {
-    try {
-      await store.dispatch('getMatches')
-      await store.dispatch('getTeams')
-      await store.dispatch('getPlayers')
-      await store.dispatch('getGoals')
-    } catch (e) {
-      console.error(e)
-    }
-  },
-  computed: {
-    ...mapState('players', ['players']),
-    ...mapState('teams', ['teams']),
-    ...mapState('matches', ['matches']),
-    ...mapState('goals', ['goals'])
-  },
-  async mounted() {
-    try {
-      await this.getMatches()
-      await this.getTeams()
-      await this.getPlayers()
-      await this.getGoals()
-    } catch (e) {
-      console.error(e)
-    }
-  },
-  methods: {
-    ...mapActions('players', ['getPlayers']),
-    ...mapActions('teams', ['getTeams']),
-    ...mapActions('matches', ['getMatches']),
-    ...mapActions('goals', ['getGoals'])
-  }
+	data() {
+		return {
+			jogo: {}
+		}
+	},
+	async fetch({ store }) {
+		try {
+			await store.dispatch('matches/getMatches')
+			await store.dispatch('teams/getTeams')
+			await store.dispatch('players/getPlayers')
+			await store.dispatch('goals/getGoals')
+		} catch (e) {
+			console.error(e)
+		}
+	},
+	computed: {
+		...mapState('players', ['players']),
+		...mapState('teams', ['teams']),
+		...mapState('matches', ['matches']),
+		...mapState('goals', ['goals'])
+	},
+	async mounted() {
+		try {
+			await this.getMatches()
+			await this.getTeams()
+			await this.getPlayers()
+			await this.getGoals()
+		} catch (e) {
+			console.error(e)
+		}
+	},
+	methods: {
+		...mapActions('players', ['getPlayers']),
+		...mapActions('teams', ['getTeams']),
+		...mapActions('matches', ['getMatches']),
+		...mapActions('goals', ['getGoals'])
+	}
 }
 </script>
